2cf5a4cda5d2563859b41b4e6917c4a47b4e4eb3
aliksab <alexeysavich1112@gmail.com>
2024-11-10 01:50:58 +0300

    fix env vite

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL='https://yvgixqrdlesydfrdvwbj.supabase.co'
 
-const supabase = createClient(SUPABASE_URL, import.meta.env.SUPABASE_API_KEY)
+const supabase = createClient(SUPABASE_URL, import.meta.env.VITE_SUPABASE_API_KEY)
+
 export default supabase
