ea8d79c10d727744e1da84a8d204aee901d8be33
aliksab <alexeysavich1112@gmail.com>
2024-11-09 18:40:22 +0300

    add SUPABASE_URL

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,4 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY)
+const SUPABASE_URL='https://yvgixqrdlesydfrdvwbj.supabase.co'
+console.log('Variables', process.env)
+
+const supabase = createClient(SUPABASE_URL, process.env.SUPABASE_API_KEY)
 export default supabase
